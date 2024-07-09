60d79593951945c54265d99936455edb3047579b
aquaman122 <swj981128@gmail.com>
2024-07-09 19:29:28 +0900

    feat: movie api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
+
+// 데코레이터 <-- 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // express 의 라우터 같은 존재
+  controllers: [AppController, MoviesController],
+  providers: [AppService, MoviesService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'hi nestjs!';
   }
+  
+  getHi(): string {
+    return 'hi Nest';
+  }
 }
